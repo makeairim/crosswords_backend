5f6548546263ac898969c4f4324e2421c4ccdd0f
Patrick <p.taesler@gmx.de>
2017-06-11 01:34:35 +0200

    moved code to methods

--- python/main/learn.py
+++ python/main/learn.py
@@ -1,48 +1,38 @@
-import numpy as np
 from sklearn.svm import LinearSVC
+import numpy as np
 import os
 import cv2
 import pickle
 
+
 def get_image(path):
     img = cv2.imread(path)
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY )
-    img = cv2.resize(img, (36,36))
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    img = cv2.resize(img, (36, 36))
     return img
 
+
+def generate_set_and_label(path):
+    set = []
+    label = []
+    for folder in os.listdir(path):
+        file_list = os.listdir(os.path.join(path, folder))
+        for filename in file_list:
+            img = get_image(os.path.join(path, folder, filename))
+            set.append(img)
+            label.append(int(folder))
+    set = np.reshape(set, (len(set), -1))
+    return set, label
+
 print("Learning...")
-path_test_set = "dataset\\test"
-testset = []
-test_label = []
-for folder in os.listdir(path_test_set):
-    flist = os.listdir(os.path.join(path_test_set, folder))
-    for f in flist:
-        img = get_image(os.path.join(path_test_set, folder, f))
-        testset.append(img)
-        test_label.append(int(folder))
-testset = np.reshape(testset, (len(testset), -1))
+test_set, test_label = generate_set_and_label("dataset\\test")
 print("...")
-
-path_train_set = "dataset\\train"
-trainset = []
-for folder in os.listdir(path_train_set):
-    flist = os.listdir(os.path.join(path_train_set, folder))
-    for f in flist:
-        img = get_image(os.path.join(path_train_set, folder, f))
-        trainset.append(img)
-trainset = np.reshape(trainset, (5000, -1))
-
-train_label = []
-for i in range(0,10):
-    temp = 500*[i]
-    train_label.extend(temp)
-
+train_set, train_label = generate_set_and_label("dataset\\train")
 print("...")
-
 clf = LinearSVC()
-clf.fit(trainset, train_label)
+clf.fit(train_set, train_label)
 
-# result = clf.predict(testset)
-print("Test accuracy: " + str(clf.score(testset, test_label)))
+print("Test accuracy: " + str(clf.score(test_set, test_label)))
 
-pickle.dump( clf, open( "recognizer.pickle", "wb" ) )
+pickle.dump(clf, open("recognizer.pickle", "wb"))
+print("Success.")
